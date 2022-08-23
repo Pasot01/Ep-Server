824f8fe74ff61ef11145f90f1744d6bf8d260715
Pasot01 <contactmyphone14@gmail.com>
2022-08-23 20:46:50 +0200

    Installation de configuration de connection db Atlas

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import mongodb from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 const app = express();
 const port = 4000;
 
+
+const uri = "mongodb+srv://pasot:Zi73TqWxFnkPPvb8@epfullstackmern.xs8acz8.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(
+    uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    });
+
+client.connect(err => {
+    console.log('Connecté avec succès à la bd');
+    // const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 // Test de routage sans requête
-app.get('/', (_,res) => {
+app.get('/', (_, res) => {
     res.send('Hello Express')
 })
 
